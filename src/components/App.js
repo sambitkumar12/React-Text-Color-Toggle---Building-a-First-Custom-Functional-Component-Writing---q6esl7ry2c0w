a930b5b0be5259a4b31585ccb77ab48c1adb4e0b
Sambit Kumar Das <92737238+sambitkumar12@users.noreply.github.com>
2023-02-14 20:50:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,17 @@ import React, {useEffect, useState} from 'react'
 import '../styles/App.css';
 
 const App = () => {
-//code here 
-  const [color, setColor] = useState('red');
-  const [fontStyle, setFontStyle] = useState('normal');
-
-  const handleClick = () => {
-    if (color === 'red') {
-      setColor('blue');
-      setFontStyle('italic');
-    } else {
-      setColor('red');
-      setFontStyle('normal');
-    }
-  };
+const [changeColor,setChangeColor]=useState(false);
+  const handleClick=()=>{
+  setChangeColor(!changeColor)
+  }
   return (
     <div id="main">
-      <p>Newton School</p>
+      <p className={`${(changeColor===true)?'blueColor':'redColor'}`} >Newton School</p>
       <button id='button' onClick={handleClick}>Change Style</button>
     </div>
-  );
-};
+  )
+}
+
 
 export default App;
